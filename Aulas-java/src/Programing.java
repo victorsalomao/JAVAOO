8bb22eba8eaff464e7c6cb57250bad9f1831899e
Victor Salomão <victor.salomaor@gmail.com>
2022-10-03 21:40:20 -0300

    Atualizei o projeto no windows 03/10 com Heranca e Polimorfismo

--- Aulas-java/src/Programing.java
+++
@@ -1,5 +0,0 @@
-public class Programing {
-  public static void main(String[] args) {
-    System.out.println("ola amiguinhas");
-  }
-}
--- Aulas-java/src/fatorial.java
+++
@@ -1,9 +0,0 @@
-public class fatorial {
-  public static void main(String[] args) {
-    int fator = 1;
-    for (int numero = 1; numero < 11; numero++) {
-      fator = fator * numero;
-      System.out.println("o fatorial de " + numero + " é o numero " + fator);
-    }
-  }
-}
